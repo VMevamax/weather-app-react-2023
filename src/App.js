ca55374db4a3829152d5ff914df8ae9ae938f8b9
VMevamax <132147870+VMevamax@users.noreply.github.com>
2023-12-29 16:38:40 +0200

    add a href

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <footer>
+        <a
+          href="https://github.com/VMevamax/weather-app-react-2023"
+          target="blank"
+        >
+          <div className="github">VM GitHub</div>
+        </a>
+      </footer>
+    </div>
+  );
 }
